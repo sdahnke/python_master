76f7fb2f4baf2183e85a92b4a696d949ffcdcb58
Steffen Dahnke <steffen.dahnke@otto.de>
2017-07-13 11:16:37 +0200

    create basket table

--- mike_matthies_spider/get_prices_from_basket.py
+++ mike_matthies_spider/get_prices_from_basket.py
@@ -10,6 +10,9 @@ br = RoboBrowser(history=False)
 login_name = "658073000"
 login_pw = "online24"
 
+basket = []
+basket_products = []
+
 br.open(url)
 forms = br.get_forms()
 for form in forms:
@@ -20,28 +23,21 @@ for form in forms:
 soup = BeautifulSoup(str(br.select), "lxml")
 
 basket_table = soup.find('table', attrs={'class':'tx_nbbasket'})
-# basket_body = basket_table.find('tbody')
-# print(basket_body)
-
-data = []
-basket = []
 
 rows = basket_table.find_all('tr')
-# print(rows)
 for row in rows:
     cells = row.find_all('td')
-    #print(cells)
-    if len(cells) > 0:
-        position = cells[1].text.strip()
-        print(position)
-    #    attr1 = cells[2].text.strip()
-    #    attr2 = cells[3].text.strip()
-    #    attr3 = cells[4].text.strip()
-
-    #    product_price = {position, attr1, attr2, attr3}
-    #    basket.append(product_price)
-
-print(basket)
+    for cell in cells:
+        cell = str(cell.text.strip())
+        if re.match("([0-9]{3}).([0-9]{2}).([0-9]{2})#w1wkg", cell):
+            search = re.search("([0-9]{3}).([0-9]{2}).([0-9]{2})#w1wkg", cell)
+            cell = search.group(1) + "." + search.group(2) + "." + search.group(3)
+        if re.match("[^0-9]+([0-9,]+)", cell):
+            search = re.search("[^0-9]+([0-9,]+)", cell)
+            cell = search.group(1)
+        basket_products.append(cell)
+    basket.append(basket)
+    basket_products = []
 
 
 # Warenkorb leeren

b9548159ed0af886a4915bf65f95d483fd3ae62f
sdahnke <steffen_info@hotmail.com>
2017-08-02 22:01:24 +0200

    add editor

--- editor/ebay_kleinanzeigen.py
+++ editor/ebay_kleinanzeigen.py
@@ -1,28 +1,24 @@
-from bs4 import BeautifulSoup
-from robobrowser import RoboBrowser
+from selenium import webdriver
 
 url = "https://www.ebay-kleinanzeigen.de/p-anzeige-aufgeben.html#?path=210/306/teile&isParent=false"
-
-br = RoboBrowser(history=False)
-
 login_name = "steffen_info@hotmail.com"
 login_pw = "online24"
 
+driver = webdriver.Chrome(executable_path='D:\chromedriver\chromedriver.exe')
+driver.get(url)
+driver.find_element_by_id("login-email").send_keys(login_name)
+driver.find_element_by_id("login-password").send_keys(login_pw)
 
-# Login-Funktion implementieren
-def ebay_k(url):
-    br.open(url)
-    form = br.get_form(id='login-form')
-    form["loginMail"] = login_name
-    form["password"] = login_pw
-    br.session.headers["Referer"] = url
-    br.submit_form(form)
+driver.find_element_by_id("login-submit").click()
 
+driver.find_element_by_id("cat_210").click()
+driver.find_element_by_id("cat_306").click()
+driver.find_element_by_id("cat_teile").click()
+driver.find_element_by_class_name("button").click()
 
-ebay_k(url)
-form = br.get_form(id="postad-step1-frm")
-br.submit_form(form)
+driver.find_element_by_id("postad-title").send_keys("Titel der Anzeige")
+driver.find_element_by_id("pstad-descrptn").send_keys("Anzeigentext")
+driver.find_element_by_id("pstad-price").send_keys("100")
+driver.find_element_by_id("priceType2").click()
 
-soup = BeautifulSoup(str(br.select), "lxml")
-
-print(soup.prettify())
+driver.find_element_by_id("pictureupload-pickfiles").click()

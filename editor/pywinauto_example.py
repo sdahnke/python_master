b949a5f0fc0fc338d62c17d65685c2917ef79a1a
sdahnke <steffen_info@hotmail.com>
2017-10-10 11:43:06 +0200

    restructure as function

--- editor/pywinauto_example.py
+++
@@ -1,9 +0,0 @@
-from pywinauto import Application
-
-## app.UntitledNotepad.menu_select("Datei->Speichern")
-## app.AboutNotepad.OK.click()
-## app.UntitledNotepad.Edit.type_keys("pywinauto Works!", with_spaces = True)
-
-app = Application().Start(cmd_line=u'"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" ')
-app.Dialog.Wait('ready')
-app.UntitledNotepad.Edit.type_keys("pywinauto Works!", with_spaces=True)

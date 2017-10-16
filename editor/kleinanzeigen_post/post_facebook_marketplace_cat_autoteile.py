7163b375982fca78085104fe2ad79f99e0090394
sdahnke <steffen_info@hotmail.com>
2017-10-16 21:49:01 +0200

    folder structure for project and facebook marketplace api

---
+++ editor/kleinanzeigen_post/post_facebook_marketplace_cat_autoteile.py
@@ -0,0 +1,25 @@
+import time
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+from splinter.driver.webdriver import BaseWebDriver
+
+
+def fb_marketplace(login_name, login_pw, title, pic_path, description, price, plz, street, company, phone):
+    url = "https://www.facebook.com"
+    # browser = Browser('chrome')
+    options = Options()
+    options.add_argument('--disable-notifications')
+    browser = BaseWebDriver()
+    browser.driver = Chrome(chrome_options=options)
+    browser.driver.set_window_size(1200, 900)
+    browser.visit(url)
+    browser.fill('email', login_name)
+    browser.fill('pass', login_pw)
+    browser.find_by_id("loginbutton").click()
+    time.sleep(6)
+    browser.find_by_text('Marketplace').click()
+
+
+fb_marketplace('steffen_info@hotmail.com', 'online24', """##TITLE##""", '##PIC##', """##DESC##""", '##PRICE##',
+               '22926', '', 'Steffen', '')
